b22f59df6a8243fa9dd29b010d7625df76bd8efe
Keyvan.Mozaffari <keyvan.mozaffari@vagani.com>
2024-01-05 08:44:34 +0300

    Initial Commit

---
+++ Swift-Apple-Framework/Swift_Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Apple_FrameworkApp.swift
+//  Swift-Apple-Framework
+//
+//  Created by Neda Mohammadi on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
